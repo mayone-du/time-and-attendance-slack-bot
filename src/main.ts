f558f9a0c5c0bbdea61a401dda0e4428b20e71ca
mayone-du <cocomayo1201@icloud.com>
2022-06-18 13:18:13 +0900

    🚧

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-const main = () => {};
+// GASはexport const ... 不可
+const CONFIG = {
+  SHEET_FILE_ID: "1sZY_rcWYvgWEHSVmXs66aQk764VxxOHObFCXKtux3ME",
+} as const;
+
+const main = () => {
+  const now = new Date();
+  const spreadSheet = SpreadsheetApp.openById(CONFIG.SHEET_FILE_ID);
+  const sheet = spreadSheet.getSheets()[0];
+  // const sheet = spreadSheet.getActiveSheet(); // 多分上と同じ
+  const cell = sheet.getRange("B3");
+  cell.setValue(now.toISOString());
+};
 
 main();
